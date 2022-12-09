8ad8df049d66492a80420d2f938f8c1364b93236
Louis-Philippe Racette <lprac@outlook.com>
2022-12-09 08:34:09 -0500

    ajout des constantes pour bullet

--- SUSV2/SUSV2/DEFINITIONS.hpp
+++ SUSV2/SUSV2/DEFINITIONS.hpp
@@ -4,3 +4,9 @@
 #define SCREEN_HEIGHT 1024
 #define LOGO_STATE_SHOW_TIME 3.0
 //#define SPLASH_STATE_BACKGROUND_FILEPATH "Ressources/res/SplashBackground.png"
+
+
+#define BULLET_FILEPATH "ressources/images/balle.png"
+#define MAIN_MENU_BACKGROUND_FILEPATH "ressources/images/menu_principale.png"
+#define RANGE_BULLET 1200
+#define SPEED_BULLET 300
